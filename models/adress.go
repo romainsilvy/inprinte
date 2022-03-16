dd2ab7e7b6f4e41585774022dd1829c60c684b6d
romainsilvy <romain.silvy06@gmail.com>
2022-03-16 13:02:17 +0100

    :hammer: code refacto for user and adress structures

--- models/adress.go
+++ models/adress.go
@@ -1,9 +1,9 @@
 package models
 
 type Adress struct {
-	street		 string   `json:"street"`
-	city         string   `json:"city"`
-	state        string   `json:"state"`
-	country      string   `json:"country "`
-	zipCode      string   `json:"zipCode"`
+	street		string	`json:"street"`
+	city		string	`json:"city"`
+	state       string	`json:"state"`
+	country     string	`json:"country "`
+	zipCode     string	`json:"zipCode"`
 }
--- models/user.go
+++ models/user.go
@@ -1,16 +1,16 @@
 package models
 
 type User struct {
-	id_user		 string   `json:"id_user"`
-	photo        string   `json:"photo"`
-	email        string   `json:"email"`
-	password     string   `json:"password "`
-	firstname    string   `json:"firstname"`
-	lastname     string   `json:"lastname"`
-	phone        string   `json:"phone"`
-	adress       []Adress `json:"adress"`
+	id_user		string		`json:"id_user"`
+	photo		string		`json:"photo"`
+	email		string		`json:"email"`
+	password	string		`json:"password "`
+	firstname	string		`json:"firstname"`
+	lastname	string		`json:"lastname"`
+	phone		string		`json:"phone"`
+	adress		[]Adress 	`json:"adress"`
 }
 
 type AllUsers struct {
-	allUsers	[]User `json:"allUsers"`
+	allUsers	[]User		`json:"allUsers"`
 }

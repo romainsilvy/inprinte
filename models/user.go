75eb7113ed27b75587b9e914528fced08a5ac7de
romainsilvy <romain.silvy06@gmail.com>
2022-03-16 12:54:14 +0100

    :zap: creating structures for user and address

---
+++ models/user.go
@@ -0,0 +1,14 @@
+type User struct {
+	id_user		 string   `json:"id_user"`
+	photo        string   `json:"photo"`
+	email        string   `json:"email"`
+	password     string   `json:"password "`
+	firstname    string   `json:"firstname"`
+	lastname     string   `json:"lastname"`
+	phone        string   `json:"phone"`
+	adress       []Adress `json:"adress"`
+}
+
+type AllUsers struct {
+	allUsers	[]User `json:"allUsers"`
+}

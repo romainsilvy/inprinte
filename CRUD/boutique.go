c9c6714a50b730e34d7831ca0034485e0efc0eb0
romainsilvy <romain.silvy06@gmail.com>
2022-03-19 22:58:55 +0100

    :hammer: code refacto for the path boutique

--- CRUD/boutique.go
+++ CRUD/boutique.go
@@ -12,94 +12,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func getNewProducts(db *sql.DB) []structures.BoutiqueProduct {
-	rows, err := db.Query("SELECT name, price, description FROM product ORDER BY id_product DESC LIMIT 3")
-
-	// check errors
-	utils.CheckErr(err)
-
-	// var response []JsonResponse
-	var newProducts []structures.BoutiqueProduct
-
-	// Foreach product
-	for rows.Next() {
-		var name string
-		var price string
-		var description string
-		//var picture string
-		err = rows.Scan(&name, &price, &description)
-
-		// check errors
-		utils.CheckErr(err)
-
-		newProducts = append(newProducts, structures.BoutiqueProduct{
-			Name:        name,
-			Price:       price,
-			Description: description,
-		})
-	}
-	return newProducts
-}
-
-func getMostWantedProduct(db *sql.DB) []structures.BoutiqueProduct {
-	rows, err := db.Query("SELECT COUNT(command_line.id_product) AS nbrOrder, name, price, description FROM product INNER JOIN command_line ON product.id_product = command_line.id_product GROUP BY command_line.id_product ORDER BY nbrOrder DESC LIMIT 3")
-
-	// check errors
-	utils.CheckErr(err)
-
-	// var response []JsonResponse
-	var mostWantedProduct []structures.BoutiqueProduct
-
-	// Foreach product
-	for rows.Next() {
-		var nbrOrder int
-		var name string
-		var price string
-		var description string
-		//var picture string
-		err = rows.Scan(&nbrOrder, &name, &price, &description)
-
-		// check errors
-		utils.CheckErr(err)
-
-		mostWantedProduct = append(mostWantedProduct, structures.BoutiqueProduct{
-			Name:        name,
-			Price:       price,
-			Description: description,
-		})
-	}
-	return mostWantedProduct
-}
-
-func getAllProducts(db *sql.DB) []structures.BoutiqueProduct {
-	rows, err := db.Query("SELECT name, price, description FROM product")
-
-	// check errors
-	utils.CheckErr(err)
-
-	// var response []JsonResponse
-	var allProducts []structures.BoutiqueProduct
-
-	// Foreach product
-	for rows.Next() {
-		var name string
-		var price string
-		var description string
-		//var picture string
-		err = rows.Scan(&name, &price, &description)
-
-		// check errors
-		utils.CheckErr(err)
-
-		allProducts = append(allProducts, structures.BoutiqueProduct{
-			Name:        name,
-			Price:       price,
-			Description: description,
-		})
-	}
-	return allProducts
-}
-
 // func getGlobalCategories(db *sql.DB) []structures.BoutiqueCategories {
 // 	rows, err := db.Query("SELECT global_category_name, category_name FROM category INNER JOIN global_category ON category.id_global_category = global_category.id_global_category")
 
@@ -127,24 +39,93 @@ func getAllProducts(db *sql.DB) []structures.BoutiqueProduct {
 // 	return categories
 // }
 
+func getNewProducts(db *sql.DB, sqlQuery string) []structures.BoutiqueProduct {
+	var name, price, description string
+	var newProducts []structures.BoutiqueProduct
+
+	rows := databaseTools.GetRequest(db, sqlQuery)
+	for rows.Next() {
+		err := rows.Scan(&name, &price, &description)
+
+		// check errors
+		utils.CheckErr(err)
+
+		newProducts = append(newProducts, structures.BoutiqueProduct{
+			Name:        name,
+			Price:       price,
+			Description: description,
+		})
+	}
+	return newProducts
+}
+
+func getMostWantedProducts(db *sql.DB, sqlQuery string) []structures.BoutiqueProduct {
+	var name, price, description string
+	var nbrOrder int
+	var mostWantedProducts []structures.BoutiqueProduct
+
+	rows := databaseTools.GetRequest(db, sqlQuery)
+	for rows.Next() {
+		err := rows.Scan(&nbrOrder, &name, &price, &description)
+
+		// check errors
+		utils.CheckErr(err)
+
+		mostWantedProducts = append(mostWantedProducts, structures.BoutiqueProduct{
+			Name:        name,
+			Price:       price,
+			Description: description,
+		})
+	}
+	return mostWantedProducts
+}
+
+func getAllProducts(db *sql.DB, sqlQuery string) []structures.BoutiqueProduct {
+	var name, price, description string
+	var allProducts []structures.BoutiqueProduct
+
+	rows := databaseTools.GetRequest(db, sqlQuery)
+	for rows.Next() {
+		err := rows.Scan(&name, &price, &description)
+
+		// check errors
+		utils.CheckErr(err)
+
+		allProducts = append(allProducts, structures.BoutiqueProduct{
+			Name:        name,
+			Price:       price,
+			Description: description,
+		})
+	}
+	return allProducts
+}
+
 func GetBoutique(w http.ResponseWriter, r *http.Request) {
 	db := databaseTools.DbConnect()
 
-	//get boutique infos\\
+	//all sql queries for this path
+	newProductsQuery := "SELECT name, price, description FROM product ORDER BY id_product DESC LIMIT 3;"
+	mostWantedProductQuery := "SELECT COUNT(command_line.id_product) AS nbrOrder, name, price, description FROM product INNER JOIN command_line ON product.id_product = command_line.id_product GROUP BY command_line.id_product ORDER BY nbrOrder DESC LIMIT 3"
+	allProductsQuery := " SELECT name, price, description FROM product;"
 
-	//get new products
-	newProducts := getNewProducts(db)
-	mostWantedProduct := getMostWantedProduct(db)
-	allProducts := getAllProducts(db)
+	//----- for the new products -----\\
+	newProducts := getNewProducts(db, newProductsQuery)
+
+	//----- for the most wanted products -----\\
+	mostWantedProducts := getMostWantedProducts(db, mostWantedProductQuery)
+
+	//----- for all the products -----\\
+	allProducts := getAllProducts(db, allProductsQuery)
+
 	// categories := getAllCategories(db)
 
+	//----- encode the json response -----\\
 	var response = structures.JsonResponseBoutique{
 		Type:               "success",
 		BoutiqueNews:       newProducts,
-		BoutiqueMostWanted: mostWantedProduct,
+		BoutiqueMostWanted: mostWantedProducts,
 		AllProducts:        allProducts,
 		// Categories:         categories,
 	}
-
 	json.NewEncoder(w).Encode(response)
 }
--- database/database.go
+++ database/database.go
@@ -1,27 +1,38 @@
 package databaseTools
 
 import (
-    "database/sql"
+	"database/sql"
+	"inprinte/backend/utils"
+
 	"github.com/fatih/color"
 
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func DbConnect() (db *sql.DB) {
-    dbDriver := "mysql"
-    dbUser := "inprinteAdmin"
-    dbPass := "louisensueur"
+	dbDriver := "mysql"
+	dbUser := "inprinteAdmin"
+	dbPass := "louisensueur"
 	dbProtocol := "tcp"
 	dbIp := "178.170.14.134"
 	dbPort := "3306"
-    dbName := "inprinte"
+	dbName := "inprinte"
 	color.Green("Connecting database ...")
 
-    db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@"+dbProtocol+"("+dbIp+":"+dbPort+")/"+ dbName)
-    if err != nil {
-        panic(err.Error())
-    } else {
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@"+dbProtocol+"("+dbIp+":"+dbPort+")/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	} else {
 		color.Cyan("Database connected !\n")
 	}
-    return db
+	return db
+}
+
+func GetRequest(db *sql.DB, sqlRequest string) *sql.Rows {
+	rows, err := db.Query(sqlRequest)
+
+	// check errors
+	utils.CheckErr(err)
+	// defer rows.Close()
+	return rows
 }

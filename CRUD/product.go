c3546fe5238a2a174a35ff832af0e0f6a89e7b63
Ayatooo <louisreynard919@gmail.com>
2022-03-19 02:44:48 +0100

    :zap: crud getProduct done

--- CRUD/product.go
+++ CRUD/product.go
@@ -1,21 +1,58 @@
 package CRUD
 
-func CreateProduct() {
+import (
+	"encoding/json"
+	"fmt"
+	databaseTools "inprinte/backend/database"
+	structures "inprinte/backend/structures"
+	utils "inprinte/backend/utils"
+	"net/http"
 
-}
+	_ "github.com/go-sql-driver/mysql"
+)
 
-func GetPrCreateProduct() {
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Getting products ...")
+	db := databaseTools.DbConnect()
 
-}
+	//Get products infos
+	rows, err := db.Query("SELECT name, description, price, AVG(stars_number) AS MOYENNE, picture.url, product_file.id_product FROM product INNER JOIN rate ON rate.id_product = product.id_product INNER JOIN product_picture ON product_picture.id_picture = product.id_product INNER JOIN picture ON picture.id_picture = product_picture.id_picture INNER JOIN product_file ON product_file.id_product = product.id_product GROUP BY product.id_product;")
 
-func UpdatePrCreateProduct() {
+	// check errors
+	utils.CheckErr(err)
 
-}
+	// var response []JsonResponse
+	var products []structures.Product
 
-func DeletePrCreateProduct() {
+	// Foreach product
+	for rows.Next() {
+		var name string
+		var description string
+		var price int
+		var stars_number float64
+		var picture_url string
+		var product_file string
 
-}
+		//var picture string
+		err = rows.Scan(&name, &description, &price, &stars_number, &picture_url, &product_file)
 
-func GetAllProducts() {
-	
+		// check errors
+		utils.CheckErr(err)
+
+		products = append(products, structures.Product{
+			Name:         name,
+			Description:  description,
+			Price:        price,
+			Stars_number: stars_number,
+			Picture_url:  picture_url,
+			Product_file: product_file,
+		})
+	}
+
+	var response = structures.JsonResponseProduct{
+		Type: "success",
+		Data: products,
+	}
+
+	json.NewEncoder(w).Encode(response)
 }

527e1011a904ba21d613d773b9e53988f15e1c7b
romainsilvy <romain.silvy06@gmail.com>
2022-03-20 22:50:01 +0100

    :zap: user path url params managed

--- CRUD/user.go
+++ CRUD/user.go
@@ -5,6 +5,7 @@ import (
 	structures "inprinte/backend/structures"
 	utils "inprinte/backend/utils"
 	"log"
+	"strings"
 
 	"encoding/json"
 	"net/http"
@@ -13,20 +14,42 @@ import (
 )
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
-	//manage keys values here
-	keys, ok := r.URL.Query()["key"]
-	if !ok || len(keys[0]) < 1 {
-		log.Println("Url Param 'key' is missing")
+	//manage url vars here
+	urlFilter, ok := r.URL.Query()["filter"]
+	if !ok || len(urlFilter[0]) < 1 {
+		log.Println("Url Param 'filter' is missing")
 		return
 	}
-	key := keys[0]
-	log.Println("Url Param 'key' is: " + string(key))
+
+	urlRange, ok := r.URL.Query()["range"]
+	if !ok || len(urlRange[0]) < 1 {
+		log.Println("Url Param 'range' is missing")
+		return
+	}
+	urlRange[0] = urlRange[0][1 : len(urlRange[0])-1]
+
+	urlSort, ok := r.URL.Query()["sort"]
+	if !ok || len(urlSort[0]) < 1 {
+		log.Println("Url Param 'sort' is missing")
+		return
+	}
+	urlSort[0] = urlSort[0][1 : len(urlSort[0])-1]
+	urlSort = strings.Split(urlSort[0], ",")
+	urlSort[0] = urlSort[0][1 : len(urlSort[0])-1]
+	urlSort[1] = urlSort[1][1 : len(urlSort[1])-1]
+
+	if urlSort[0] == "id" {
+		urlSort[0] = "id_user"
+	}
+
+	// filters := filter[0]
+	log.Println("Url Params are : " + string(urlFilter[0]) + string(urlRange[0]) + string(urlSort[0]))
 
 	db := databaseTools.DbConnect()
 
 	//all sql queries for this path
-	sqlQuery := "SELECT id_user, email, password, first_name AS firstname, last_name AS lastname, phone FROM user"
-
+	sqlQuery := "SELECT id_user, email, password, first_name AS firstname, last_name AS lastname, phone FROM user ORDER BY " + urlSort[0] + " " + urlSort[1] + " LIMIT " + urlRange[0]
+	log.Println(sqlQuery)
 	var id_user int
 	var email, password, firstname, lastname, phone string
 	var allUsers []structures.User

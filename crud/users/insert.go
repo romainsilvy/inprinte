daf92db732d8ef7ceec2ab90af69d9e3d2924776
romainsilvy <romain.silvy06@gmail.com>
2022-03-26 17:07:08 +0100

    :zap: add cors header for insert

--- crud/users/insert.go
+++ crud/users/insert.go
@@ -1,11 +1,12 @@
 package crud
 
 import (
+	utils "inprinteBackoffice/utils"
 	"log"
 	"net/http"
 )
 
 func Insert(w http.ResponseWriter, r *http.Request) {
+	utils.SetCorsHeaders(&w)
 	log.Println("Inserting user")
-	log.Println(w)
 }

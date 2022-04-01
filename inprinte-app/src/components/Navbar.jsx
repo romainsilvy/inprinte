4d0cc9bc3bddc702e2253ea38f23c28c278917e2
Matteo <matteo.dinville@ynov.com>
2022-04-01 09:58:57 +0200

    :zap: header grid done without path animation

--- inprinte-app/src/components/Navbar.jsx
+++ inprinte-app/src/components/Navbar.jsx
@@ -9,40 +9,30 @@ export const Navbar = () => {
     return (
         <>
             <nav className="nav">
-                <div className="nav-container">
-                    <div className="left">
-                        <Link to="/">
-                            <img src={InprinteLogo} alt="Inprinte Logo" />
-                        </Link>
+                    <Link to="/" className="left">
+                        <img src={InprinteLogo} alt="Inprinte Logo" />
+                    </Link>
+                    <ul className="middle">
+                        <li>
+                            <Link to="/">Accueil</Link>
+                        </li>
+                        <li>
+                            <Link to="/boutique">Boutique</Link>
+                        </li>
+                        <li>
+                            <Link to="/propositions">Propositions</Link>
+                        </li>
+                        <li>
+                            <Link to="/demande">Demande</Link>
+                        </li>
+                        <li>
+                            <Link to="/Contact">Contact</Link>
+                        </li>
+                    </ul>
+                    <div className="right"> 
+                            <Link to='/connexion'><img className="con" src={ConnexionLogo} alt="Connexion Logo" /></Link>
+                            <Link to='/panier'><img className="cart" src={CartLogo} alt="Cart Logo" /></Link>
                     </div>
-                    <div className="middle">
-                        <ul>
-                            <li>
-                                <Link to="/accueil">Accueil</Link>
-                            </li>
-                            <li>
-                                <Link to="/boutique">Boutique</Link>
-                            </li>
-                            <li>
-                                <Link to="/propositions">Propositions</Link>
-                            </li>
-                            <li>
-                                <Link to="/demande">Demande</Link>
-                            </li>
-                            <li>
-                                <Link to="/Contact">Contact</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="right">
-                        <ul>
-                            <li>
-                                <Link to='/connexion'><img className="con" src={ConnexionLogo} alt="Connexion Logo" /></Link>
-                                <Link to='/panier'><img className="cart" src={CartLogo} alt="Cart Logo" /></Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
             </nav>
         </>
     );

0677fbc03a9ebdeefe07f71a9464634e24e3aa87
Matteo <matteo.dinville@ynov.com>
2022-04-04 09:18:18 +0200

    :zap: add footer

--- inprinte-app/src/App.js
+++ inprinte-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar } from './components/Navbar';
-// import Footer from './components/Footer'
+import { Footer } from './components/Footer'
 import Accueil from './pages/Accueil.jsx'
 import Boutique from './pages/Boutique'
 import Propositions from './pages/Propositions'
@@ -15,17 +15,18 @@ function App() {
   return (
     <div className="app">
       <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<div style= {{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}><h1>Bienvenue chez Inprinte</h1></div>} />
-            <Route path="/accueil" element={<Accueil/>} />
-            <Route path="/boutique" element={<Boutique/>} />
-            <Route path="/propositions" element={<Propositions/>} />
-            <Route path="/demande" element={<Demande/>} />
-            <Route path="/contact" element={<Contact/>} />
-            <Route path="/connexion" element={<Login/>} />
-            <Route path="/panier" element={<Panier/>} />
-          </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}><h1>Bienvenue chez Inprinte</h1></div>} />
+          <Route path="/accueil" element={<Accueil />} />
+          <Route path="/boutique" element={<Boutique />} />
+          <Route path="/propositions" element={<Propositions />} />
+          <Route path="/demande" element={<Demande />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/connexion" element={<Login />} />
+          <Route path="/panier" element={<Panier />} />
+        </Routes>
+        <Footer />
       </Router>
     </div>
   );
--- inprinte-app/src/components/Footer.jsx
+++ inprinte-app/src/components/Footer.jsx
@@ -1,64 +1,57 @@
 import React from 'react';
-import {
-    Container,
-    Row,
-    Column,
-    FooterLink,
-    Heading,
-} from "../styles/FooterStyles";
-
-const Footer = () => {
+import InprinteLogo from '../images/logo.svg'
+import FooterStyle from "../styles/FooterStyle.css";
+import { Link } from "react-router-dom";
+import bar from '../images/bar.svg'
+import facebook from '../images/facebook.svg'
+import twitter from '../images/twitter.svg'
+import instagram from '../images/instagram.svg'
+export const Footer = () => {
     return (
-        <div className="footer">
-            <Container>
-                <Row>
-                    <Column>
-                        <Heading>A Propos De Nous</Heading>
-                        <FooterLink href="/F.A.Q">F.A.Q</FooterLink>
-                        <FooterLink href="/mentions-légales">Mentions légales</FooterLink>
-                    </Column>
-                    <Column>
-                        <Heading>Nos Services</Heading>
-                        <FooterLink href="/accueil">Accueil</FooterLink>
-                        <FooterLink href="/boutique">Boutique</FooterLink>
-                        <FooterLink href="/propositions">Propositions</FooterLink>
-                        <FooterLink href="/profil">Profil</FooterLink>
-                        <FooterLink href="/panier">Panier</FooterLink>
-                    </Column>
-                    <Column>
-                        <Heading>Contact</Heading>
-                        <FooterLink href="/tel">04 XX XX XX XX </FooterLink>
-                        <FooterLink href="/mobile">08 XX XX XX XX</FooterLink>
-                        <FooterLink href="/email">email@email.com</FooterLink>
-                        <FooterLink href="/adresse">27 rue raoul servant, 69007 Lyon FRANCE</FooterLink>
-                    </Column>
-                    <Column>
-                        <Heading>Communauté</Heading>
-                        <FooterLink href="https://facebok.com">
-                            <i className="fab fa-facebook-f">
-                                <span style={{ marginLeft: "10px" }}>
-                                    Facebook
-                                </span>
-                            </i>
-                        </FooterLink>
-                        <FooterLink href="https://instagram.com">
-                            <i className="fab fa-instagram">
-                                <span style={{ marginLeft: "10px" }}>
-                                    Instagram
-                                </span>
-                            </i>
-                        </FooterLink>
-                        <FooterLink href="https://twitter.com">
-                            <i className="fab fa-twitter">
-                                <span style={{ marginLeft: "10px" }}>
-                                    Twitter
-                                </span>
-                            </i>
-                        </FooterLink>
-                    </Column>
-                </Row>
-            </Container>
-        </div>
+        <>
+            <footer>
+                <div className="container">
+                    <Link className="logo" to="/"><img src={InprinteLogo} alt="Inprinte Logo" /></Link>
+                    <div className="propos">
+                        <h2>A propos de nous</h2>
+                        <ul>
+                            <li><Link to="/">FAQ</Link></li>
+                            <li><Link to="/">Mentions Légales</Link></li>
+                        </ul>
+                    </div>
+                    <div className="services">
+                        <h2>Nos Services</h2>
+                        <ul>
+                            <li><Link to="/">Accueil</Link></li>
+                            <li><Link to="/">Boutique</Link></li>
+                            <li><Link to="/">Propositions</Link></li>
+                            <li><Link to="/">Profil</Link></li>
+                            <li><Link to="/">Panier</Link></li>
+                        </ul>
+                    </div>
+                    <div className="contact">
+                        <h2>Contact</h2>
+                        <ul>
+                            <li><Link to="/">Telephone</Link></li>
+                            <li><Link to="/">email</Link></li>
+                            <li><Link to="/">addresse</Link></li>
+                        </ul>
+                    </div>
+                    <div className="communauté">
+                        <h2>Communauté</h2>
+                        <ul>
+                            <li><Link to="/"><img className="bar" src={instagram} alt="instagram" /></Link></li>
+                            <li><Link to="/"><img className="bar" src={facebook} alt="facebook" /></Link></li>
+                            <li><Link to="/"><img className="bar" src={twitter} alt="twitter" /></Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </footer>
+            <img className="bar" src={bar} alt="bar" />
+            <div className="copyrightText">
+                <p>Copyright &copy; 2022 Tous droits réservés</p>
+            </div>
+        </>
     );
 };
 

e3ea5692e3e20f3920925de7b6c64f98fbb8f541
romainsilvy <romain.silvy06@gmail.com>
2022-04-05 23:56:11 +0200

    :zap: main part of the shop is done

--- inprinte-app/src/pages/Boutique.jsx
+++ inprinte-app/src/pages/Boutique.jsx
@@ -1,12 +1,185 @@
 
 import React from 'react';
+import '../styles/Boutique.css'
+import { Navbar } from '../components/Navbar';
+import { Footer } from '../components/Footer';
+import { ShopProduct } from '../components/ShopProduct';
+import loading from '../images/loading.svg'
 
-const Boutique = () => {
-    return (
-        <div style= {{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}>
-           <h1>Boutique</h1> 
-        </div>
-    );
-};
+
+
+
+class Boutique extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8080/boutique")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return (
+            <div className="Boutique">
+                <Navbar />
+                <h1 className="shopTitle">
+                    Shoppez par catégories
+                </h1>
+                <div className="allCategories">
+                    <p className="category">cat1</p>
+                    <p className="category">cat1</p>
+                    <p className="category">cat1</p>
+                    <p className="category">cat1</p>
+                </div>
+                <div className="allShopProducts">
+                    <img className="loading" src={loading} alt="loading" />
+                </div>
+                <Footer />
+            </div>  
+            )
+            
+
+        return (
+            <div className="Boutique">
+                <Navbar />
+                <h1 className="shopTitle">
+                    Shoppez par catégories
+                </h1>
+                <div className="allCategories">
+                    <p className="category">cat1</p>
+                    <p className="category">cat1</p>
+                    <p className="category">cat1</p>
+                    <p className="category">cat1</p>
+                </div>
+                <div className="allShopProducts">
+                {
+                    items.AllProducts.map((item) => (
+                        <ShopProduct product={item}/>
+                    ))
+                }
+                </div>
+                <Footer />
+            </div> 
+            
+        )
+    }
+}
+
+{/* <div className = "Boutique">
+                <h1> Fetch data from an api in react </h1>  {
+                    items.BoutiqueNews.map((item) => (
+                        <ShopProduct product={item}/>
+                    ))
+                }
+            </div> */}
+
+
+
+
+
+
+{/* <div className="Boutique">
+                <Navbar />
+                <h1 className="shopTitle">
+                    Shoppez par catégories
+                </h1>
+                <div className="allCategories">
+                    <p className="category">cat1</p>
+                    <p className="category">cat1</p>
+                    <p className="category">cat1</p>
+                    <p className="category">cat1</p>
+                </div>
+                <div className="allShopProducts">
+
+                items.map((item) => ( 
+                <ol key = { item.id } >
+                    User_Name: { item.username }, 
+                    Full_Name: { item.name }, 
+                    User_Email: { item.email } 
+                    </ol>
+                ))
+                    <ShopProduct product={product}/>
+                    <ShopProduct/>
+                    <ShopProduct/>
+                    <ShopProduct/>
+                </div>
+                <Footer />
+            </div> */}
+
 
 export default Boutique;
+//     let allProducts = fetch('http://localhost:8080/boutique')
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((result) => {
+//         allProducts = result.AllProducts
+//         return allProducts
+//     })
+
+
+//     allProducts.then((a) => {
+//         a.map(product => (
+//             console.log(product.Name)
+//         ))
+//     })
+
+//     return (
+//         <div className="">
+//             <Navbar />
+//             <h1 className="shopTitle">
+//                 Shoppez par catégories
+//             </h1>
+//             <div className="allCategories">
+//                 <p className="category">cat1</p>
+//                 <p className="category">cat1</p>
+//                 <p className="category">cat1</p>
+//                 <p className="category">cat1</p>
+//             </div>
+//             <div className="allShopProducts">
+
+//                 {}
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+//                 <ShopProduct/>
+
+//             </div>
+//             <Footer />
+//         </div>
+//     );
+// }

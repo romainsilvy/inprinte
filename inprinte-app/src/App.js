fda819fe31a45d445209c2586b623eb6a9b6c8e7
Matteo <matteo.dinville@ynov.com>
2022-04-11 22:17:45 +0200

    :zap: update page profil without data

--- inprinte-app/src/App.js
+++ inprinte-app/src/App.js
@@ -1,32 +1,29 @@
-import React from 'react'
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Navbar } from './components/Navbar';
-import { Footer } from './components/Footer'
-import Accueil from './pages/Accueil.jsx'
-import Boutique from './pages/Boutique'
-import Propositions from './pages/Propositions'
-import Demande from './pages/Demande'
-import Contact from './pages/Contact'
-import Login from './pages/Login'
-import Panier from './pages/Panier'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import Accueil from "./pages/Accueil.jsx";
+import Boutique from "./pages/Boutique";
+import Propositions from "./pages/Propositions";
+import Demande from "./pages/Demande";
+import Contact from "./pages/Contact";
+import Profil from "./pages/Profil";
+import Panier from "./pages/Panier";
 
 function App() {
   return (
     <div className="app">
       <Router>
-        <Navbar />
         <Routes>
-          <Route path="/" element={<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}><h1>Bienvenue chez Inprinte</h1></div>} />
-          <Route path="/accueil" element={<Accueil />} />
+          <Route path="/" element={<Accueil />} />
           <Route path="/boutique" element={<Boutique />} />
           <Route path="/propositions" element={<Propositions />} />
           <Route path="/demande" element={<Demande />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/connexion" element={<Login />} />
+          <Route path="/profil" element={<Profil />} />
           <Route path="/panier" element={<Panier />} />
         </Routes>
-        <Footer />
       </Router>
     </div>
   );
---
+++ inprinte-app/src/pages/Profil.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import '../styles/Profil.css'
+import { render } from '@testing-library/react';
+import { Navbar } from '../components/Navbar';
+import { Footer } from '../components/Footer';
+import bar2 from '../images/bar2.svg'
+import random from '../images/random.jpg'
+
+const Profil = () => {
+    render(); {
+        return (
+            <div className="">
+                <Navbar />
+                <div className="textInfo">
+                    <div className="info">
+                        <p>Nom / Prénom</p>
+                        <p>Addrese mail</p>
+                        <p className="adresse">Adresse</p>
+                    </div>
+                </div>
+                <div className="commandline">
+                    <div className="titleCat1">
+                        <h1>Historique des commandes</h1>
+                        <img className="bar2" src={bar2} alt="bar" />
+                    </div>
+                    <div className="commandline1">
+                        <img className="commandPicture" src={random} alt="image" />
+                        <div className="infoCommand">
+                            <h2>Commande N°13569385</h2>
+                            <p>Expédié le 12/12/12</p>
+                            <p>Description</p>
+                        </div>
+                        <div className="status">
+                            <p>Statut de la commande</p>
+                        </div>
+                    </div>
+                    <div className="commandline2">
+                        <img className="commandPicture" src={random} alt="image" />
+                        <div className="infoCommand">
+                            <h2>Commande N°13569385</h2>
+                            <p>Expédié le 12/12/12</p>
+                            <p>Description</p>
+                        </div>
+                        <div className="status">
+                            <p>Statut de la commande</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="favorite">
+                    <div className="titleCat2">
+                        <h1>Liste de mes favoris</h1>
+                        <img className="bar2" src={bar2} alt="bar" />
+                    </div>
+                    <div className="cartFavorite1">
+                        <h2>Name</h2>
+                        <img className="pictureCart" src={random} alt="image" />
+                        <div className="infos">
+                            <p clasName="price">100€</p>
+                        </div>
+                    </div>
+                    <div className="cartFavorite2">
+                        <h2>Name</h2>
+                        <img className="pictureCart" src={random} alt="image" />
+                        <div className="infos">
+                            <p clasName="price">100€</p>
+                        </div>
+                    </div>
+                    <div className="cartFavorite3">
+                        <h2>Name</h2>
+                        <img className="pictureCart" src={random} alt="image" />
+                        <div className="infos">
+                            <p clasName="price">100€</p>
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        );
+    };
+}
+
+export default Profil;

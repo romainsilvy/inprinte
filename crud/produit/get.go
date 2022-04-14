3c3489cf8bd2d5246df51fe507d2e297e5cb18e4
romainsilvy <romain.silvy06@gmail.com>
2022-04-14 14:43:55 +0200

    :zap: product picture problem fixed

--- crud/produit/get.go
+++ crud/produit/get.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	structures "inprinte/backend/structures"
 	utils "inprinte/backend/utils"
+	"strconv"
 
 	"net/http"
 
@@ -76,7 +77,7 @@ func getRelatedProduct(r *http.Request, db *sql.DB, id_product string) []structu
 	rangeBy := utils.GetRangeParam(r)
 
 	//execute the sql query and check errors
-	rows, err := db.Query("SELECT product.id, product.name, product.price, picture.url, AVG(rate.stars_number) AS rate FROM product INNER JOIN product_picture ON product_picture.id = product.id INNER JOIN picture ON picture.id = product_picture.id INNER JOIN rate ON rate.id_product = product.id WHERE product.is_alive = true AND product.pending_validation = false AND product.id_category = (SELECT category.id FROM category INNER JOIN product ON product.id_category = category.id WHERE product.id = ?) GROUP BY product.id ORDER BY rate DESC ?", id_product, rangeBy)
+	rows, err := db.Query("SELECT product.id, product.name, product.price, AVG(rate.stars_number) AS rate FROM product INNER JOIN rate ON rate.id_product = product.id WHERE product.is_alive = true AND product.pending_validation = false AND product.id_category = (SELECT category.id FROM category INNER JOIN product ON product.id_category = category.id WHERE product.id = " + id_product + " ) GROUP BY product.id ORDER BY rate DESC " + rangeBy)
 	utils.CheckErr(err)
 
 	//parse the query
@@ -87,8 +88,9 @@ func getRelatedProduct(r *http.Request, db *sql.DB, id_product string) []structu
 		var price, rate float64
 
 		//retrieve the values and check errors
-		err = rows.Scan(&id, &name, &price, &picture, &rate)
+		err = rows.Scan(&id, &name, &price, &rate)
 		utils.CheckErr(err)
+		picture = getProductPicture(db, strconv.Itoa(id))
 
 		//add the values to the response
 		productRelated = append(productRelated, structures.ProductRelated{
@@ -102,6 +104,29 @@ func getRelatedProduct(r *http.Request, db *sql.DB, id_product string) []structu
 
 	//create the json response
 	return productRelated
+}
+
+func getProductPicture(db *sql.DB, id_product string) string {
+	//global vars
+	// var product_picture []string
+	var url string
+	//create the sql query
+	sqlQuery := ("SELECT picture.url FROM picture INNER JOIN product_picture ON product_picture.id_picture = picture.id INNER JOIN product ON product.id = product_picture.id_product WHERE product.id = " + id_product + " LIMIT 1;")
+
+	err := db.QueryRow(sqlQuery).Scan(&url)
+	// rows, err := db.Query(sqlQuery)
+	utils.CheckErr(err)
+
+	// for rows.Next() {
+	// 	//retrieve the values and check errors
+	// 	err = rows.Scan(&url)
+	// 	utils.CheckErr(err)
+
+	// 	//add the values to the response
+	// 	product_picture = append(product_picture, url)
+	// }
+
+	return url
 }
 
 func Get(w http.ResponseWriter, r *http.Request) {

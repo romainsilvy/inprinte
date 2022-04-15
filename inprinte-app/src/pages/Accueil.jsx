038525a5e25e9f8726aafd175102ec7ac826f5be
Ayatooo <louisreynard919@gmail.com>
2022-04-15 21:02:09 +0200

    :zap: accueil full fixed

--- inprinte-app/src/pages/Accueil.jsx
+++ inprinte-app/src/pages/Accueil.jsx
@@ -39,87 +39,87 @@ class Accueil extends React.Component {
     render() {
         const { DataisLoaded, items } = this.state;
         if (!DataisLoaded) return <div>
-            <img className="loading" src={loading} alt="loading" /></div>;
+            <img className="Accueilloading" src={loading} alt="loading" /></div>;
 
         return (
-            <div className="">
+            <div className="Accueil">
                 <Navbar />
-                <div className="contentOne">
-                    <img className="donut" src={donut} alt="icon" />
-                    <div className="blur">
-                        <div className="glass-panel">
-                            <p className="titleglass">Bienvenue <span> !</span></p>
-                            <p className="subtitle">Voici le <span>meilleur site </span>d'impression et de livraison de <span>modèles 3D.</span></p>
-                            <p className="desc">Nous sommes une jeune entreprise <span>dynamique </span> à votre disposition ! Nous vous proposons tout
-                                un <span>catalogue </span> de modèles 3D à imprimer et à vous <span>faire livrer.</span> N'hésitez pas à découvrir également
-                                le top des articles de la semaine ou des plus vendus. <span>Qu'attendez vous ?!</span></p>
+                <div className="AccueilcontentOne">
+                    <img className="Accueildonut" src={donut} alt="icon" />
+                    <div className="Accueilblur">
+                        <div className="Accueilglass-panel">
+                            <p className="Accueiltitleglass">Bienvenue <span className="Accueilblue"> !</span></p>
+                            <p className="Accueilsubtitle">Voici le <span className="Accueilpurple">meilleur site </span>d'impression et de livraison de <span>modèles 3D.</span></p>
+                            <p className="Accueildesc">Nous sommes une jeune entreprise <span className="Accueilpink">dynamique </span> à votre disposition ! Nous vous proposons tout
+                                un <span className="Accueilblue">catalogue </span> de modèles 3D à imprimer et à vous <span className="Accueilpurple">faire livrer.</span> N'hésitez pas à découvrir également
+                                le top des articles de la semaine ou des plus vendus. <span className="Accueilpink">Qu'attendez vous ?!</span></p>
                         </div>
                     </div>
-                    <div className="btn1">
-                        <Link to="/propositions"><img className="btnPropositions" src={buttonPropositions} alt="btn" /></Link>
+                    <div className="Accueilbtn1">
+                        <Link to="/propositions"><img className="AccueilbtnPropositions" src={buttonPropositions} alt="btn" /></Link>
                     </div>
-                    <img className="imageIso" src={imageIso} alt="img" />
+                    <img className="AccueilimageIso" src={imageIso} alt="img" />
                 </div>
-                <div className="contentTwo">
-                    <img className="losange" src={losange} alt="icon" />
-                    <img className="boule" src={boule} alt="icon" />
-                    <img className="serp" src={serp} alt="icon" />
-                    <div className="cardTop2">
-                        <h1>Top<span className="purple">#2</span></h1>
-                        <img className="pictureCardTop2" src={items.mostSales[1].Picture} alt="image" />
-                        <div className="infos">
-                            <p className="name">{items.mostSales[1].Name}</p>
-                            <p className="price">{items.mostSales[1].Price}€</p>
+                <div className="AccueilcontentTwo">
+                    <img className="Accueillosange" src={losange} alt="icon" />
+                    <img className="Accueilboule" src={boule} alt="icon" />
+                    <img className="Accueilserp" src={serp} alt="icon" />
+                    <div className="AccueilcardTop2">
+                        <h1>Top<span className="Accueilpurple">#2</span></h1>
+                        <img className="AccueilpictureCardTop2" src={items.mostSales[1].Picture} alt="image" />
+                        <div className="Accueilinfos">
+                            <p className="Accueilname">{items.mostSales[1].Name}</p>
+                            <p className="Accueilprice">{items.mostSales[1].Price}€</p>
                         </div>
                     </div>
-                    <div className="cardTop1">
-                        <h1>Top<span className="pink">#1</span> des ventes</h1>
-                        <img className="pictureCardTop1" src={items.mostSales[0].Picture} alt="image" />
-                        <div className="infos">
-                            <p className="name">{items.mostSales[0].Name}</p>
-                            <p className="price">{items.mostSales[0].Price}€</p>
+                    <div className="AccueilcardTop1">
+                        <h1>Top<span className="Accueilpink">#1</span> des ventes</h1>
+                        <img className="AccueilpictureCardTop1" src={items.mostSales[0].Picture} alt="image" />
+                        <div className="Accueilinfos">
+                            <p className="Accueilname">{items.mostSales[0].Name}</p>
+                            <p className="Accueilprice">{items.mostSales[0].Price}€</p>
                         </div>
                     </div>
-                    <div className="cardTop3">
-                        <h1>Top<span className="green">#3</span></h1>
-                        <img className="pictureCardTop3" src={items.mostSales[2].Picture} alt="image" />
-                        <div className="infos">
-                            <p className="name">{items.mostSales[2].Name}</p>
-                            <p className="price">{items.mostSales[2].Price}€</p>
+                    <div className="AccueilcardTop3">
+                        <h1>Top<span className="Accueilblue">#3</span></h1>
+                        <img className="AccueilpictureCardTop3" src={items.mostSales[2].Picture} alt="image" />
+                        <div className="Accueilinfos">
+                            <p className="Accueilname">{items.mostSales[2].Name}</p>
+                            <p className="Accueilprice">{items.mostSales[2].Price}€</p>
                         </div>
                     </div>
-                    <div className="btn2">
-                        <a href="/boutique"><img className="btnBoutique" src={btnBoutique} alt="btn" /></a>
+                    <div className="Accueilbtn2">
+                        <a href="/boutique"><img className="AccueilbtnBoutique" src={btnBoutique} alt="btn" /></a>
                     </div>
                 </div>
-                <div className="contentTree">
-                    <div className="titleCat">
+                <div className="AccueilcontentTree">
+                    <div className="AccueiltitleCat">
                         <h1>Le top de la semaine</h1>
-                        <img className="bar2" src={bar2} alt="bar" />
+                        <img className="Accueilbar2" src={bar2} alt="bar" />
                     </div>
-                    <div className="cart1">
-                        <h2>{items.bestRated[0].Name}</h2>
-                        <img className="pictureCart" src={items.bestRated[0].Picture} alt="image" />
-                        <div className="infos">
-                            <p className="price">{items.bestRated[0].Price}€</p>
+                    <div className="Accueilcart1">
+                        <h2 className="pink">{items.bestRated[0].Name}</h2>
+                        <img className="AccueilpictureCart" src={items.bestRated[0].Picture} alt="image" />
+                        <div className="Accueilinfos">
+                            <p className="Accueilprice">{items.bestRated[0].Price}€</p>
                         </div>
                     </div>
-                    <div className="cart2">
-                        <h2>{items.bestRated[1].Name}</h2>
-                        <img className="pictureCart" src={items.bestRated[1].Picture} alt="image" />
-                        <div className="infos">
-                            <p className="price">{items.bestRated[1].Price}€</p>
+                    <div className="Accueilcart2">
+                        <h2 className="purple">{items.bestRated[1].Name}</h2>
+                        <img className="AccueilpictureCart" src={items.bestRated[1].Picture} alt="image" />
+                        <div className="Accueilinfos">
+                            <p className="Accueilprice">{items.bestRated[1].Price}€</p>
                         </div>
                     </div>
-                    <div className="cart3">
-                        <h2>{items.bestRated[2].Name}</h2>
-                        <img className="pictureCart" src={items.bestRated[2].Picture} alt="image" />
-                        <div className="infos">
-                            <p className="price">{items.bestRated[2].Price}€</p>
+                    <div className="Accueilcart3">
+                        <h2 className="Accueilblue">{items.bestRated[2].Name}</h2>
+                        <img className="AccueilpictureCart" src={items.bestRated[2].Picture} alt="image" />
+                        <div className="Accueilinfos">
+                            <p className="Accueilprice">{items.bestRated[2].Price}€</p>
                         </div>
                     </div>
-                    <div className="btn3">
-                        <a href="/boutique"><img className="btnBoutique" src={btnBoutique} alt="btn" /></a>
+                    <div className="Accueilbtn3">
+                        <a href="/boutique"><img className="AccueilbtnBoutique" src={btnBoutique} alt="btn" /></a>
                     </div>
                 </div>
                 <Footer />
